56718762fdfcb9983061ea82fddec20e5c80d025
komalarao <manoharvakamullu1999@gmail.com>
2022-11-15 01:15:37 -0800

    day3-User Case problem 1

---
+++ IsPresent.java
@@ -0,0 +1,15 @@
+package bridgelabz;
+
+public class IsPresent {
+
+	
+	   public static void CheckPresent() {
+		double chek= Math.floor(Math.random()*10)%2;
+		if(chek == 1) {
+			System.out.println("employee is present");
+		}else {
+			System.out.println("employee is abscent");
+		}
+	
+	}
+	}
